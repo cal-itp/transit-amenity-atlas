62373c79cfa87b93f560bfde86634458a4db1dc5
V <vevetron@gmail.com>
2026-01-07 14:43:57 -0800

    name it right

--- stop_ids/boundary_gdf_builder.py
+++ stop_ids/boundary_gdf_builder.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 from shapely.geometry import box
 import pandas as pd
 
-df = pd.read_csv("full_latest_stops.csv")
+df = pd.read_csv("../full_latest_stops.csv", low_memory=False)
 
 # iterate through unique geohashes and create bounding box polygons
 data = []
@@ -12,4 +12,5 @@ for geohash in df['geohash_8'].unique():
     poly = box(bbox.min_lon, bbox.min_lat, bbox.max_lon, bbox.max_lat)
     data.append({"geohash": geohash, "geometry": poly})
 gdf = gpd.GeoDataFrame(data, crs="EPSG:4326")
-gdf.to_file("hash_8_boundary_output.json", driver="GeoJSON")
+gdf.to_file("../data/hash_8_boundary_output.fgb", engine="pyogrio")
+# gdf.to_file("output.fgb", engine="pyogrio")
