6c09a644fc070130d8c7159acb7fe05f02abdde6
V <156962295+vevetron@users.noreply.github.com>
2026-03-13 15:07:21 -0700

    Some updates (#60)
    * adjusted precision to precision 7
    
    * Adds gkpg format and updates data dictionary
    
    * Script to build visualization of boundaries over all geohashes
    
    * name it right
    
    * multiple precisions
    
    * cleanup
    
    ---------
    
    Co-authored-by: V <vevetron@gmail.com>

---
+++ stop_ids/boundary_gdf_builder.py
@@ -0,0 +1,26 @@
+import pygeohash as pgh
+import geopandas as gpd
+from shapely.geometry import box
+import pandas as pd
+
+df = pd.read_csv("../full_latest_stops.csv", low_memory=False)
+
+for each_precision in [7,8,9]:
+    geohashes = set()
+
+    for index, row in df.iterrows():
+        lat = row['stop_lat']
+        lon = row['stop_lon']
+        
+        # Generate the geohash with each precision
+        geohash = pgh.encode(lat, lon, precision=each_precision)
+        
+        geohashes.add(geohash)
+
+    data = []
+    for geohash in geohashes:
+        bbox = pgh.get_bounding_box(geohash)
+        poly = box(bbox.min_lon, bbox.min_lat, bbox.max_lon, bbox.max_lat)
+        data.append({"geohash": geohash, "geometry": poly})
+    gdf = gpd.GeoDataFrame(data, crs="EPSG:4326")
+    gdf.to_file(f"../data/hash_{each_precision}_boundary_output.fgb", engine="pyogrio")
